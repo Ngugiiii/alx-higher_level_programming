7451a80d07b74cc08e34643dab9eb3382c5353ec
Ngugiiii <certifiedngugi@gmail.com>
2023-08-16 13:32:23 -0700

    sqlll

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,4 @@
+--  lists the number of records with the same score in the table
 SELECT score, name
 FROM second_table
 HAVING name IS NOT NULL
--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,3 +1,4 @@
+-- creates a table second_table in the database
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 
 -- Script should create maximum of 4 records
